61742e1c51dd691c54f17bc52b1cd29606f2a6f8
makolon <smakolon385@gmail.com>
2026-01-10 23:10:09 +0900

    Fix

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,102 +1,84 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+local map = vim.keymap.set
+
 -- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Open a file via Telescope and reveal it in Neo-tree
-vim.keymap.set('n', '<leader>ef', function()
-  local builtin = require 'telescope.builtin'
-  builtin.find_files {
-    attach_mappings = function(_, map)
-      map('i', '<CR>', function(prompt_bufnr)
-        local actions = require 'telescope.actions'
-        local action_state = require 'telescope.actions.state'
-        local entry = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
+-- --------------------------------------------------------------------
+-- Explorer / Finder (Oil + Telescope)
+-- --------------------------------------------------------------------
 
-        -- Open the selected file
-        vim.cmd('edit ' .. entry.path)
+-- Find files with Telescope (simple, no Neo-tree reveal)
+map('n', '<leader>ef', function()
+  require('telescope.builtin').find_files()
+end, { desc = 'Find file (Telescope)' })
 
-        -- Reveal the file location in Neo-tree
-        vim.cmd 'Neotree reveal'
-      end)
-      return true
-    end,
-  }
-end, { desc = 'Find file and reveal in Neo-tree' })
+-- Open Oil in the current file's directory (fallback to cwd if no file)
+map('n', '<leader>ed', function()
+  local ok, oil = pcall(require, 'oil')
+  if not ok then
+    vim.notify('oil.nvim is not available', vim.log.levels.WARN)
+    return
+  end
 
--- Open Telescope file browser, and reveal the selected directory in Neo-tree
-vim.keymap.set('n', '<leader>ed', function()
-  local fb = require('telescope').extensions.file_browser
-  fb.file_browser {
-    prompt_title = 'Browse Directories',
-    select_buffer = true,
-    depth = 1,
-    hijack_netrw = false,
-    attach_mappings = function(_, map)
-      map('i', '<CR>', function(prompt_bufnr)
-        local actions = require 'telescope.actions'
-        local action_state = require 'telescope.actions.state'
-        local entry = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
+  local bufname = vim.api.nvim_buf_get_name(0)
+  local dir
+  if bufname == '' then
+    dir = vim.fn.getcwd()
+  else
+    dir = vim.fn.fnamemodify(bufname, ':p:h')
+  end
 
-        -- If selected is a file, extract its parent directory
-        local path = entry.path or entry.filename
-        local dir = vim.fn.isdirectory(path) == 1 and path or vim.fn.fnamemodify(path, ':h')
+  oil.open(dir)
+end, { desc = 'Open directory (Oil)' })
 
-        -- Reveal that directory in Neo-tree
-        vim.cmd('Neotree reveal dir=' .. dir)
-      end)
-      return true
-    end,
-  }
-end, { desc = 'Browse and reveal directory in Neo-tree' })
+-- (Optional) If you want a dedicated key for opening Oil at cwd:
+-- map('n', '<leader>eD', function()
+--   local ok, oil = pcall(require, 'oil')
+--   if ok then oil.open(vim.fn.getcwd()) end
+-- end, { desc = 'Open cwd (Oil)' })
 
--- Move between buffers (VSCode-style tab switching)
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Close current buffer' })
+-- --------------------------------------------------------------------
+-- Buffers
+-- --------------------------------------------------------------------
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- If sidekick.nvim is installed, avoid mapping <Tab>/<S-Tab> in normal mode
+-- because sidekick often uses <Tab> for suggestions.
+local has_sidekick = pcall(require, 'sidekick')
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+if not has_sidekick then
+  -- Move between buffers (VSCode-style tab switching)
+  map('n', '<Tab>', ':bnext<CR>', { desc = 'Next buffer' })
+  map('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Previous buffer' })
+else
+  -- Safe defaults when sidekick exists
+  map('n', ']b', ':bnext<CR>', { desc = 'Next buffer' })
+  map('n', '[b', ':bprevious<CR>', { desc = 'Previous buffer' })
+end
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<leader>bd', ':bdelete<CR>', { desc = 'Close current buffer' })
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+-- Exit terminal mode in the builtin terminal
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- --------------------------------------------------------------------
+-- Window navigation
+-- --------------------------------------------------------------------
+
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- --------------------------------------------------------------------
 -- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+-- --------------------------------------------------------------------
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -105,8 +87,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Bufferline keymaps
-vim.keymap.set('n', '<leader>wl', '<CMD>BufferLineCloseRight<CR>')
-vim.keymap.set('n', '<leader>wh', '<CMD>BufferLineCloseLeft<CR>')
-vim.keymap.set('n', '<leader>wall', '<CMD>BufferLineCloseOthers<CR>')
-vim.keymap.set('n', '<leader>we', '<CMD>BufferLinePickClose<CR>')
+-- --------------------------------------------------------------------
+-- Bufferline keymaps (keep if you use bufferline)
+-- --------------------------------------------------------------------
+
+map('n', '<leader>wl', '<CMD>BufferLineCloseRight<CR>', { desc = 'BufferLine: close to the right' })
+map('n', '<leader>wh', '<CMD>BufferLineCloseLeft<CR>', { desc = 'BufferLine: close to the left' })
+map('n', '<leader>wall', '<CMD>BufferLineCloseOthers<CR>', { desc = 'BufferLine: close others' })
+map('n', '<leader>we', '<CMD>BufferLinePickClose<CR>', { desc = 'BufferLine: pick close' })
+
